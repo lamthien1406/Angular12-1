17da3b1221b85127586be3634c91a6b4067b7502
Thien <lamthien1406@gmail.com>
2021-08-12 21:04:00 +0700

    menu

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  opened: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  toggleSidebar(){
+    this.opened = !this.opened;
+  }
 }
